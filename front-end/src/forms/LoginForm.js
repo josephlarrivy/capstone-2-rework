f78020aeb8c762b59a8bddd2b8a50fcbfbd9ff4a
josephlarrivy <joseph.larrivy@gmail.com>
2023-03-30 08:23:37 -0500

    working on allowing sharing to twitter/facebook

--- front-end/src/forms/LoginForm.js
+++ front-end/src/forms/LoginForm.js
@@ -40,34 +40,37 @@ const LoginForm = ({ setToken }) => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
+        <>
+            <form onSubmit={handleSubmit}>
 
-            <label htmlFor="username" className="col-md-6">username: </label>
-            <input
-                required
-                id={formData.username}
-                type="text"
-                name="username"
-                placeholder="username"
-                value={formData.username}
-                onChange={handleChange}
-                className="col-md-6"
-            /><br></br><br></br>
+                <label htmlFor="username" className="col-md-6">username: </label>
+                <input
+                    required
+                    id={formData.username}
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    className="col-md-6"
+                /><br></br><br></br>
 
-            <label htmlFor="password" className="col-md-6">password: </label>
-            <input
-                required
-                id={formData.password}
-                type="text"
-                name="password"
-                placeholder="password"
-                value={formData.password}
-                onChange={handleChange}
-                className="col-md-6"
-            /><br></br><br></br>
+                <label htmlFor="password" className="col-md-6">password: </label>
+                <input
+                    required
+                    id={formData.password}
+                    type="text"
+                    name="password"
+                    placeholder="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="col-md-6"
+                /><br></br><br></br>
 
-            <button>Submit</button>
-        </form>
+                <button>Submit</button>
+            </form>
+        </>
+        
     )
 }
 
