c8112075fe0bdb2ec4dbfb8d82dd59ea4119facd
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-09 22:15:39 -0500

    IMage Dispalyer starting to work

--- front-end/src/components/ImagesDisplayer.js
+++ front-end/src/components/ImagesDisplayer.js
@@ -1,29 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import "../css/ImagesDisplayer.css";
 
-import '../css/ImagesDisplayer.css'
+const ImagesDisplayer = ({ imagesArray }) => {
 
-const ImagesDisplayer = ({imagesArray}) => {
+  const [containerState, setContainerState] = useState('images-displayer-main-container')
+  const [images, setImages] = useState(imagesArray);
 
-  const [images, setImages] = useState(imagesArray)
+  const handleMouseEnter = (e) => {
+    e.currentTarget.className = "single-image-container-open";
+    const otherDivs = document.querySelectorAll(
+      "#images-displayer-main-container > div.single-image-container:not(:hover)"
+    );
+    otherDivs.forEach((div) => {
+      div.className = "single-image-container-with-one-open";
+    });
+    setContainerState('images-displayer-main-container-with-one-open')
+    console.log('mouseenter')
+  };
 
-  useEffect(() => {
-    console.log(imagesArray)
-  }, [])
-
-
-
+  const handleMouseLeave = (e) => {
+    // e.currentTarget.className = "single-image-container";
+    const otherDivs = document.querySelectorAll(
+      "#images-displayer-main-container > div"
+    );
+    otherDivs.forEach((div) => {
+      div.className = "single-image-container";
+    });
+    setContainerState('images-displayer-main-container')
+    console.log('mouseleave')
+  };
 
   return (
-    <div id="images-displayer-main-container">
-      {images && images.map(image => {
-        return (
-          <div className="single-image-container">
-            <img className="single-image" src={image.url}></img>
-          </div>
-        )
+    <div id={containerState} onMouseLeave={handleMouseLeave}>
+      {images &&
+        images.map((image) => {
+          return (
+            <div
+              key={image.url}
+              className="single-image-container"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img 
+                src={image.url}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                />
+            </div>
+          );
         })}
     </div>
-  )
-}
+  );
+};
 
 export default ImagesDisplayer;
