32985a148313a8552b1bc09ac07499b4f9b09630
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-15 08:07:23 -0500

    setting up TripItem model

---
+++ back-end/models/tripItem.js
@@ -0,0 +1,18 @@
+
+const db = require('../db')
+const { ExpressError, NotFoundError, UnauthorizedError, BadRequestError, ForbiddenError } = require('../ExpressError')
+
+
+
+
+class TripItem {
+
+  static async add (
+    { route, name, description, park }
+  )
+
+}
+
+
+
+module.exports = TripItem
