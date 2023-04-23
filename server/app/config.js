a57203914c2cbdb523318192b6ff1ea2760ff1b4
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-23 11:52:52 -0500

    changesd database URI

--- server/app/config.js
+++ server/app/config.js
@@ -9,10 +9,16 @@ const SECRET_KEY = process.env.SECRET_KEY || "doNotTell";
 const PORT = +process.env.PORT || 3001;
 
 
+// function getDatabaseUri() {
+//     return (process.env.NODE_ENV === "test")
+//         ? "mapApplicationDB-testing"
+//         : process.env.DATABASE_URL || "mapApplicationDB";
+// }
+
 function getDatabaseUri() {
     return (process.env.NODE_ENV === "test")
         ? "mapApplicationDB-testing"
-        : process.env.DATABASE_URL || "mapApplicationDB";
+        : process.env.DATABASE_URL || "postgres://nsfltstxzljrmo:20be57f36b56cc624bcbad1174fdce9eea005763eb29535b067caf3025744cb5@ec2-44-215-22-37.compute-1.amazonaws.com:5432/d5uoit9fc1t0c7";
 }
 
 
